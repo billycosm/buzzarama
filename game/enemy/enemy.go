54e570399f6c125fbf74b81a51f6d3c625c99156
billycosm <william.j.coleman-smith@bath.edu>
2026-06-30 10:32:54 +0100

    update enemy atlas and default animation

--- game/enemy/enemy.go
+++ game/enemy/enemy.go
@@ -1,7 +1,7 @@
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"enemy_64_64\"\n"
+  data: "default_animation: \"bot_sketch_64_64\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
